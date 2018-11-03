2c3262bcb196ce9dde2df19b8fd4548a3184d153
ekramulmostafa <ekramul@bongobd.com>
2018-11-03 15:56:46 +0600

    redux building

--- src/components/StackList.js
+++ src/components/StackList.js
@@ -1,16 +1,24 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import stacks from '../data/stacks.json'
+import { connect } from 'react-redux'
+import { setStack } from '../action'
+import { bindActionCreators } from 'redux'
 
-class Stacks extends Component {
+class StackList extends Component {
     render(){
+        console.log('stacklist props', this.props)
         return(
             <div>
                 {
                     stacks.map(stack => {
                         return(
-                            <Link to='/stack'>
-                                <h4 key={stack.id}>{stack.title}</h4>
+                            <Link
+                                to='/stack'
+                                key={ stack.id }
+                                onClick={()=>this.props.setStack(stack)}
+                            >
+                                <h4>{stack.title}</h4>
                             </Link>
                         )
                     })
@@ -20,4 +28,10 @@ class Stacks extends Component {
     }
 }
 
-export default Stacks;
+function mapDispatchTOProps(dispatch){
+    return bindActionCreators({setStack}, dispatch)
+}
+
+const connectComponent = connect(null, mapDispatchTOProps)
+
+export default connectComponent(StackList);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import { Provider } from 'react-redux'
 import App from './components/App'
 import Stack from './components/Stack'
 import { createStore } from 'redux'
 import rootReducer from './reducer'
+import { setStack } from './action'
+
+const store = createStore(rootReducer)
+store.subscribe(() => console.log('store', store.getState()))
+
+store.dispatch(setStack({id: 0, title: 'example', card: []}))
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route exact path='/' component={App} />
-            <Route path='/stack' component={Stack} />
-        </Switch>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={App} />
+                <Route path='/stack' component={Stack} />
+            </Switch>
+        </BrowserRouter>
+    </Provider>
+    ,
     document.getElementById('root'))
